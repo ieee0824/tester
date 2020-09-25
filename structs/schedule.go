d8fd9c87cc511691e835e04cefab6e7b04abe77e
ieee0824 <ieee0824x@gmail.com>
2020-09-26 07:36:10 +0900

    add debug mode, fix cookie jar

--- structs/schedule.go
+++ structs/schedule.go
@@ -3,12 +3,27 @@ package structs
 import "fmt"
 
 type Schedule struct {
-	Jobs []Job
+	debug bool
+	Jobs  []Job
+}
+
+func (s *Schedule) EnableDebugMode() {
+	s.debug = true
+}
+
+func (s *Schedule) DisableDebugMode() {
+	s.debug = false
 }
 
 func (s *Schedule) Run() error {
+	var opt []JobOption
+	if s.debug {
+		opt = []JobOption{
+			{Debug: true},
+		}
+	}
 	for _, j := range s.Jobs {
-		if err := j.Run(); err != nil {
+		if err := j.Run(opt...); err != nil {
 			return fmt.Errorf("Error: job name: %s, error: %v", j.Name, err)
 		}
 	}
